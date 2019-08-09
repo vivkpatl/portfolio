be44c9b4af16c70592b2f2253d7bf5dd13318936
Vivek Patel <therealvivek9064@gmail.com>
2019-08-09 02:14:52 -0700

    Updates

---
+++ precache-manifest.7a47dc1b4ccd0bb8ea3bb25d62fb3096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ad9b588e431ae775f19574167dc2f78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573deeac2cfd33ba0cf1",
+    "url": "/static/css/main.5902185c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "573deeac2cfd33ba0cf1",
+    "url": "/static/js/main.b6068ab5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
